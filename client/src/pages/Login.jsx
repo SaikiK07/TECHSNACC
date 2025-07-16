bac2ca3caff9ce7bcc49a057750893e546209cfe
SaikiK07 <axatparmar7@gmail.com>
2025-07-16 20:29:00 +0530

    client3

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -155,10 +155,18 @@ const Login = () => {
             {/* Google Login */}
             <div className="text-center mt-4">
               <p className="mb-2 text-gray-500">Or login in with Google</p>
-              <GoogleLogin
-                onSuccess={handleGoogleSuccess}
-                onError={() => toast.error("Google login failed")}
-              />
+              <div className="flex justify-center">
+                <GoogleLogin
+                  onSuccess={handleGoogleSuccess}
+                  onError={() => toast.error("Google login failed")}
+                  width="100%"
+                  theme="outline"
+                  size="large"
+                  text="signin_with"
+                  shape="rectangular"
+                />
+              </div>
+
             </div>
           </form>
         </div>
